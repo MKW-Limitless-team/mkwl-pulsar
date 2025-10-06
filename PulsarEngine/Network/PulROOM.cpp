a269c90569153ab8d715e34920f39cb0fe56a0cf
Zakaria Hayaty <104063230+Blazico1@users.noreply.github.com>
2025-10-06 16:13:39 +0200

    fixed incorrect gp duration bug

--- PulsarEngine/Network/PulROOM.cpp
+++ PulsarEngine/Network/PulROOM.cpp
@@ -44,22 +44,25 @@ static void BeforeROOMSend(RKNet::PacketHolder<PulROOM>* packetHolder, PulROOM* 
         u8 raceCount;
         if (koSetting == KOSETTING_ENABLED) raceCount = 0xFE;
         else switch (settings.GetSettingValue(Settings::SETTINGSTYPE_HOST, SETTINGHOST_SCROLL_GP_RACES)) {
-        case(0x2):
+        case(0):  // HOSTSETTING_GP_RACES_4
+            raceCount = 3;
+            break;
+        case(1):  // HOSTSETTING_GP_RACES_8
             raceCount = 7;
             break;
-        case(0x4):
+        case(2):  // HOSTSETTING_GP_RACES_12
             raceCount = 11;
             break;
-        case(0x6):
+        case(3):  // HOSTSETTING_GP_RACES_24
             raceCount = 23;
             break;
-        case(0x8):
+        case(4):  // HOSTSETTING_GP_RACES_32
             raceCount = 31;
             break;
-        case(0xA):
+        case(5):  // HOSTSETTING_GP_RACES_64
             raceCount = 63;
             break;
-        case(0xC):
+        case(6):  // HOSTSETTING_GP_RACES_2
             raceCount = 1;
             break;
         default:
