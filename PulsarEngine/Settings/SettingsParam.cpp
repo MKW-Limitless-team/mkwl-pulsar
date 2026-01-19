c7cf61b4797fdb2cff43275a347cf3dea04f3429
Blazico <zico2002@hotmail.nl>
2026-01-19 15:58:29 +0100

    Implemented Input Display

--- PulsarEngine/Settings/SettingsParam.cpp
+++ PulsarEngine/Settings/SettingsParam.cpp
@@ -8,14 +8,14 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    4, 5, 3, 5, 2 //menu, race, host, OTT, KO
-
+    5, 5, 3, 5, 2 //menu, race, host, OTT, KO
 };
+
 u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2 }; //menu, race, host, OTT, KO
 
 u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //first row is PulsarSettingsType, 2nd is rowIdx of radio
 {
-    { 2, 2, 3, 2, 0, 0 }, //Menu
+    { 2, 2, 3, 2, 3, 0 }, //Menu
     { 2, 2, 2, 2, 3, 0 }, //Race
     { 2, 4, 2, 0, 0, 0 }, //Host
     { 3, 3, 2, 2, 2, 0 }, //OTT
@@ -24,7 +24,7 @@ u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //fi
 
 u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount] =
 {
-    { 5, 7, 0, 0, 0}, //Menu 
+    { 5, 7, 0, 0, 0}, //Menu
     { 4, 0, 0, 0, 0}, //Race
     { 7, 0, 0, 0, 0}, //Host
     { 0, 0, 0, 0, 0}, //OTT
@@ -33,6 +33,3 @@ u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount
 
 }//namespace Settings
 }//namespace Pulsar
-
-
-
