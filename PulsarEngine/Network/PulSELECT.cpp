c1e4c1ba6d32c8d329cfffca17a27d7390176dcf
Zakaria Hayaty <zico2002@hotmail.nl>
2026-09-16 22:19:42 +0200

    Bug fix: 200cc WW?

--- PulsarEngine/Network/PulSELECT.cpp
+++ PulsarEngine/Network/PulSELECT.cpp
@@ -201,6 +201,7 @@ static void DecideCC(ExpSELECTHandler& handler) {
         const RKNet::RoomType roomType = controller->roomType;
         u8 ccClass = 1; //1 100, 2 150, 3 mirror
         if (roomType == RKNet::ROOMTYPE_VS_REGIONAL
+            || roomType == RKNet::ROOMTYPE_JOINING_REGIONAL
             || roomType == RKNet::ROOMTYPE_FROOM_HOST && ccSetting == HOSTSETTING_CC_NORMAL) {
             Random random;
             const u32 result = random.NextLimited(100); //25
